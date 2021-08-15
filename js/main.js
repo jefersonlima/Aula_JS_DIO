5be7777a3c224c6160eb56edea697f69aa05ac1d
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-08-15 06:13:14 -0400

    Aula 3 - Condicionais, laços de repetição e Date

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+//AULA 3
+
+var data = new Date();
+alert(data);
+alert(data.getDay()); //get day deu pau verificar!
+alert(data.getHours());
+alert(data.getMinutes());
+
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+
+var count = 0;
+while (count <= 5){
+    //console.log(count);
+    alert(count)
+    //count = count + 1;
+    count++;
+}
+
+var idade = prompt("Qual sua idade");
+//var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+
+/*
 //AULA 2
 //DICIONARIOS
 var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"Uva", cor:"roxa"}]
@@ -20,7 +50,7 @@ console.log(lista.reverse());
 console.log(lista.toString());
 
 console.log(lista.join( " | "))
-
+*/
 /*
 //AULA 1
 var nome = "Jeferson Lima";
